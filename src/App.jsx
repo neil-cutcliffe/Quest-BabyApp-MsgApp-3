54ce707fbf312483975172372dd89ad4bcecb6d1
Neil Cutcliffe <neil.cutcliffe@deepspacesparkle.com>
2023-11-03 10:35:15 -0700

    Quest Nov 3 - 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/QuestUiKitLightTheme';
-import MsgApp2 from 'src/components/MsgApp2/MsgApp2';
 
 function App() {
   return (
     <HelmetProvider>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={questTheme}>
-          <Routes>
-            <Route path="/" element={<MsgApp2 />} />
-          </Routes>
+          <Routes></Routes>
         </ThemeProvider>
       </StyledEngineProvider>
     </HelmetProvider>
